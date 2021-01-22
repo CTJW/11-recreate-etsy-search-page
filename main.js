fa576e28348fef4c251da4668b8d1ab476f2a747
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-01-21 20:41:13 -0500

    worked on nav, need help with spacing

--- main.js
+++ main.js
@@ -5,15 +5,33 @@ const theProducts = items.results;
 const theProductsHTML = theProducts.map(function (item) {
   return `
     <div class = "product">
-    <img src="${item.Images[0].url_fullxfull}">
-    <h2>
- ${item.title} ${item.price}
-    </h2>
+    <img src="${item.Images[0].url_fullxfull}" class ="product-bg">
+ <p class="title">${item.title}</p> 
+ <p class="shop">${item.Shop.shop_name}</p>
+  <p class="price">${item.price}</p>
+   
     </div>`;
+});
+
+let nav = [
+  `${"Valentine's Day"}  
+  ${"Jewelry & Accessories"} 
+  ${"Clothing"}
+  ${"Home & Living"}
+  ${"Wedding & Party"}
+  ${"Toys & Entertainment"}
+  ${"Art & Collectibles"}
+  ${"Craft & Supplies"},
+  ${"Gifts & Gift Cards"}`,
+];
+
+let navTitles = nav.map(function (titles) {
+  return `<li>${titles}</li>`;
 });
 
 // console.log(theProducts1)
 
+document.querySelector("#navTitles").innerHTML = nav.join("");
 document.querySelector("#grid").innerHTML = theProductsHTML.join("");
 
 {
