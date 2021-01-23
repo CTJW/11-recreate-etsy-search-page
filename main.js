8589ce40ca47230c33621066267ee59559e753d8
Candice James-Waples <76015275+CTJW@users.noreply.github.com>
2021-01-23 18:55:18 -0500

    attempted to update hover and stars

--- main.js
+++ main.js
@@ -8,14 +8,26 @@ const theProductsHTML = theProducts.map(function (item) {
     <img src="${item.Images[0].url_fullxfull}" class ="product-bg">
  <p class="title">${item.title}</p> 
  <p class="shop">${item.Shop.shop_name}</p>
-  <p class="price">${item.price}</p>
+ 
+        <div class="stars">
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        <span class="fa fa-star"></span>
+        </div>
+
+        <span class="views"> views (${item.views})</span>
+      
+ 
+  <p class="price"><strong>${item.price}</strong></p>
    
     </div>`;
 });
 
 let nav = [
   "Valentine's Day",
-  "Jewelry & Accessories",
+  "Jewelry & Accessories</div>",
   "Clothing",
   "Home & Living",
   "Wedding & Party",
@@ -29,22 +41,23 @@ let navTitles = nav.map(function (titles) {
   return `<li>${titles}</li>`;
 });
 
-let dropTitles = [
-  "All Jewelry & Accessories",
-  "Bags & Purses",
-  "Necklaces",
-  "Rings",
-  "Earrings",
-  "Bracelets",
-  "Body Jewelry",
-  "All Jewelry",
-];
+// let dropTitles = [
+//   "All Jewelry & Accessories",
+//   "Bags & Purses",
+//   "Necklaces",
+//   "Rings",
+//   "Earrings",
+//   "Bracelets",
+//   "Body Jewelry",
+//   "All Jewelry",
+// ];
 
-let dropDown = dropTitles.map(function (titles) {
-  return `<li>${titles}</li>`;
-});
+// let dropDown = dropTitles.map(function (titles) {
+//   return `<li>${titles}</li>`;
+// });
 
-document.querySelector(".dropDown").innerHTML = dropDown.join("");
+// document.querySelector(".dropDown").innerHTML = dropDown.join("");
+
 document.querySelector("#navTitles").innerHTML = navTitles.join("");
 document.querySelector("#grid").innerHTML = theProductsHTML.join("");
 
